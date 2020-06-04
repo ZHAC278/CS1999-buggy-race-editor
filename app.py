d173e7460f01b8562e311edc607e3d1c7801d1f9
Ellie Waldie-Dicken <zhac278@live.rhul.ac.uk>
2020-06-04 14:40:56 +0100

    Adding validation on wheel number

--- app.py
+++ app.py
@@ -26,27 +26,33 @@ def create_buggy():
     return render_template("buggy-form.html")
   elif request.method == 'POST':
     msg=""
+    flag_color = request.form['flag_color'].strip()
+    flag_color_secondary = request.form['flag_color_secondary']
+    flag_pattern = request.form['flag_pattern']
+    qty_wheels = request.form['qty_wheels']
+    if not qty_wheels.isdigit():
+        msg = f"This is not a number!:{qty_wheels}"
+        return render_template("buggy-form.html", msg = msg)
+    elif int(qty_wheels)%2 != 0:
+        msg = f"Wheels need to be an even number!:{qty_wheels}"
+        return render_template("buggy-form.html", msg = msg)
+    qty_tyres = request.form['qty_tyres']
+    tyres = request.form['tyres']
+    power_type = request.form['power_type']
+    power_units = request.form['power_units']
+    aux_power_type = request.form['aux_power_type']
+    aux_power_units = request.form['aux_power_units']
+    hamster_booster = request.form['hamster_booster']
+    armour = request.form['armour']
+    attack  = request.form['attack']
+    qty_attacks = request.form['qty_attacks']
+    fireproof = request.form['fireproof']
+    insulated = request.form['insulated']
+    antibiotic = request.form['antibiotic']
+    banging = request.form['banging']
+    algo = request.form['algo']
+    #msg = f"flag-color={flag_color}", #"flag_color_secondary={flag_color_secondary}", "flag_pattern={flag_pattern}", "qty_wheels={qty_wheels}", "qty_tyres={qty_tyres}", "tyres={tyres}", "power_type={power_type}", "power_units={power_units}", "aux_power_type={aux_power_type}", "aux_power_units={aux_power_units}"
     try:
-      flag_color = request.form['flag_color']
-      flag_color_secondary = request.form['flag_color_secondary']
-      flag_pattern = request.form['flag_pattern']
-      qty_wheels = request.form['qty_wheels']
-      qty_tyres = request.form['qty_tyres']
-      tyres = request.form['tyres']
-      power_type = request.form['power_type']
-      power_units = request.form['power_units']
-      aux_power_type = request.form['aux_power_type']
-      aux_power_units = request.form['aux_power_units']
-      hamster_booster = request.form['hamster_booster']
-      armour = request.form['armour']
-      attack  = request.form['attack']
-      qty_attacks = request.form['qty_attacks']
-      fireproof = request.form['fireproof']
-      insulated = request.form['insulated']
-      antibiotic = request.form['antibiotic']
-      banging = request.form['banging']
-      algo = request.form['algo']
-      msg = f"flag-color={flag_color}", #"flag_color_secondary={flag_color_secondary}", "flag_pattern={flag_pattern}", "qty_wheels={qty_wheels}", "qty_tyres={qty_tyres}", "tyres={tyres}", "power_type={power_type}", "power_units={power_units}", "aux_power_type={aux_power_type}", "aux_power_units={aux_power_units}"
       with sql.connect(DATABASE_FILE) as con:
         cur = con.cursor()
         cur.execute("UPDATE buggies set flag_color=?, flag_color_secondary=?, flag_pattern=?, qty_wheels=?, qty_tyres=?, tyres=?, power_type=?, power_units=?, aux_power_type=?, aux_power_units=?, hamster_booster=?, armour=?, attack=?, qty_attacks=?, fireproof=?, insulated=?, antibiotic=?, banging=?, algo=? WHERE id=?", 
